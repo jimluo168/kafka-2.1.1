fc43ef137403bde9920b866d0fd85f493941cc51
luojm <jimluo168@163.com>
2020-06-18 16:14:24 +0800

    修复在windows上运行 清理日志文件被另外一个进程占用的问题

---
+++ build.bat
@@ -0,0 +1,8 @@
+gradlew -PscalaVersion=2.12 jar
+
+./gradlew --info releaseTarGzAll -x signArchives
+
+# 编译为scala 2.11 版本
+./gradlew -PscalaVersion=2.11 releaseTarGz
+# 编译为 2.12 版本
+./gradlew -PscalaVersion=2.12 releaseTarGz
